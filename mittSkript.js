3fcc8d5d3ff01679bb36587b0b2c3e6a89524700
Bea Olsson <beatrice-olsson@hotmail.com>
2025-01-17 15:55:23 +0100

    Uppdated layout of index bock part along with footer. Added and created uppdate of disagn on media.

--- mittSkript.js
+++
@@ -1,8 +0,0 @@
-function dropDownFunction() {
-    const menyDropDown=document.getElementsByClassName("drop-down")[0];
-    if(menyDropDown.style.display === "none"){
-        menyDropDown.style.display="block";
-    }else{
-        menyDropDown.style.display="none";
-    }
-};
--- mittSkript1.js
+++ mittSkript1.js
@@ -2,7 +2,18 @@ function dropDownFunction() {
     const menyDropDown=document.getElementsByClassName("drop-down")[0];
     if(menyDropDown.style.display === "none"){
         menyDropDown.style.display="block";
-    }else{
+    } else {
         menyDropDown.style.display="none";
+    }
+};
+
+function readText() {
+    const textContainer=document.getElementById("text");
+    if(textContainer.classList.contains("open")){
+        textContainer.classList.remove("open");
+        document.querySelector(".read-more").innerText = "Läs Mer";
+    } else {
+        textContainer.classList.add("open");
+        document.querySelector(".read-more").innerText = "Läs Mindre";
     }
 };
