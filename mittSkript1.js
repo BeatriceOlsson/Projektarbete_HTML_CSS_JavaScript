0d9f91bf5dd3f205d2b0b7d2c4e6231a565a25e0
Bea Olsson <beatrice-olsson@hotmail.com>
2025-01-31 09:02:31 +0100

    Added image change on book_page and form in JS. Fix on from in JS.

--- mittSkript1.js
+++ mittSkript1.js
@@ -1,21 +1,40 @@
-function dropDownFunction() {
-    const menyDropDown=document.getElementsByClassName("drop-down")[0];
-    console.log(menyDropDown.style);
-    if(menyDropDown.style.display === "block"){
+function dropDownFunction() { //Dropdown function i menyn
+    if(menyDropDown.style.display === "block"){     //Kollar status kopplat till stilen om det är licla med block
         menyDropDown.style.display="none";
     } else {
         menyDropDown.style.display="block";
     }
 };
+const menyDropDown=document.getElementsByClassName("drop-down")[0];    //hemtar status av drop down. [0] behövs för att hämta datta
 
-function readText() {
+function readText() { //Read more på index
     const textContainer=document.getElementById("text");
-    if(textContainer.classList.contains("open")){
+    if(textContainer.classList.contains("open")){   //Kollar statusen på classen kopplad till id="text"
         textContainer.classList.remove("open");
-        document.querySelector(".read-more").innerText = "Läs Mer";
+        document.querySelector(".read-more").innerText = "Läs Mer"; //Stänger samt ändrar text i knapp kopplad till class
     } else {
-        textContainer.classList.add("open");
+        textContainer.classList.add("open");//Upnar drop down 
         document.querySelector(".read-more").innerText = "Läs Mindre";
     }
 };
 
+//Bute av bild på book_page
+const imgBook = document.getElementById("bookImage");
+
+const images = [
+    "Bilder/36544276.jpg",
+    "Bilder/7963.jpg"
+];
+let imageShowing=0;
+
+imgBook?.addEventListener("click", () => {
+    imageShowing = (imageShowing +1) % images.length; //Adderar och delar suman för att se om suman är licka med antal bilder
+    imgBook.src = images[imageShowing]; //Byter till den bilden som motsvara plaseringen i array
+});
+
+
+const userSumitButtom=document.getElementById("about_me_form");
+userSumitButtom?.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    alert('Tack för ditt medelande');
+});
